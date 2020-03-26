bef432b01c4a26034fc78d385f25d84b0c7b479c
Andreea Nastase <andreanastase@gmail.com>
2020-03-26 16:52:01 +0100

    Add text to initialize

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import { parseMarkdown } from './parseMarkdown';
+import marked from 'marked';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdownInput: '',
+      markdownInput:
+        'Delete or edit this text as needed:\n\n# Heading 1\n\n## Heading 2\n\nMarkdown was created by [John Gruber with help from Aaron Swartz](https://daringfireball.net/projects/markdown/)\n\nUse single backticks `to represent inline code`\n\nUse triple backticks for code blocks:\n\n```\n<script>\nalert( `Hello, world!` );\n</script>\n```\n\nUse hyphens (dashes) to indicate list items:\n\n- An item\n\n- Another item\n\n- A third item\n\nUse the greater than sign to indicate a blockquote:\n\n> This is a quoted paragraph\n\nYou can add images too:\n\n![Image of the Markdown mark](https://raw.githubusercontent.com/dcurtis/markdown-mark/master/png/66x40.png)\n\nAnd make things **strong** or _emphasize_ them.',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,21 +19,24 @@ class App extends React.Component {
   }
 
   render() {
+    let markdownOutput = { __html: `${marked(this.state.markdownInput)}` };
+
     return (
       <div>
-        <h1>Convert your Markdown</h1>
-        <div id="container">
-          <div>
-            <h2>Input:</h2>
-            <textarea
-              id="editor"
-              value={this.state.markdownInput}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <h2>Output:</h2>
-            <p>{parseMarkdown(this.state.markdownInput)}</p>
+        <h1 id="title">Convert your Markdown</h1>
+
+        <div>
+          <h2>Input:</h2>
+          <textarea
+            id="editor"
+            value={this.state.markdownInput}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <h2>Output:</h2>
+          <div id="preview">
+            <p dangerouslySetInnerHTML={markdownOutput}></p>
           </div>
         </div>
       </div>
