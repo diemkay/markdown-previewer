c8501ce9244ded66dbc7f48e2fcd4fa888c58fef
Andreea Nastase <andreanastase@gmail.com>
2020-03-27 14:59:12 +0100

    Change styles, add copy to clipboard functionality

--- src/index.js
+++ src/index.js
@@ -8,38 +8,66 @@ class App extends React.Component {
     super(props);
     this.state = {
       markdownInput:
-        'Delete or edit this text as needed:\n\n# Heading 1\n\n## Heading 2\n\nMarkdown was created by [John Gruber with help from Aaron Swartz](https://daringfireball.net/projects/markdown/)\n\nUse single backticks `to represent inline code`\n\nUse triple backticks for code blocks:\n\n```\n<script>\nalert( `Hello, world!` );\n</script>\n```\n\nUse hyphens (dashes) to indicate list items:\n\n- An item\n\n- Another item\n\n- A third item\n\nUse the greater than sign to indicate a blockquote:\n\n> This is a quoted paragraph\n\nYou can add images too:\n\n![Image of the Markdown mark](https://raw.githubusercontent.com/dcurtis/markdown-mark/master/png/66x40.png)\n\nAnd make things **strong** or _emphasize_ them.',
+        '(Delete or edit this text as needed to see the preview on the right hand side)\n\n# Heading 1\n\n## Heading 2\n\nMarkdown was created by [John Gruber with help from Aaron Swartz](https://daringfireball.net/projects/markdown/)\n\nUse single backticks `to represent inline code`\n\nUse triple backticks for code blocks:\n\n```\n<script>\nalert( `Hello, world!` );\n</script>\n```\n\nUse hyphens (dashes) to indicate list items:\n\n- An item\n\n- Another item\n\n- A third item\n\nUse the greater than sign to indicate a blockquote:\n\n> This is a quoted paragraph\n\nYou can add images too:\n\n![Image of the Markdown mark](https://raw.githubusercontent.com/dcurtis/markdown-mark/master/png/66x40.png)\n\nAnd make things **strong** or _emphasize_ them.',
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.copyToClipboard = this.copyToClipboard.bind(this);
   }
 
   handleChange(event) {
     this.setState({ markdownInput: event.target.value });
   }
 
+  async copyToClipboard(event) {
+    let copyText = document.getElementById('markdown-text').innerHTML;
+    try {
+      await navigator.clipboard.writeText(copyText);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
-    let markdownOutput = { __html: `${marked(this.state.markdownInput)}` };
+    let markdownText = marked(this.state.markdownInput);
+    let markdownOutput = { __html: `${markdownText}` };
 
     return (
-      <div>
-        <h1 id="title">Convert your Markdown</h1>
-
-        <div>
-          <h2>Input:</h2>
-          <textarea
-            id="editor"
-            value={this.state.markdownInput}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <h2>Output:</h2>
-          <div id="preview">
-            <p dangerouslySetInnerHTML={markdownOutput}></p>
+      <React.Fragment>
+        <header className="row">
+          <h1>A simple Markdown Previewer</h1>
+        </header>
+        <main className="row">
+          <div className="col-md-6">
+            <h4>Input</h4>
+            <textarea
+              type="text"
+              id="editor"
+              value={this.state.markdownInput}
+              onChange={this.handleChange}
+            />
           </div>
-        </div>
-      </div>
+          <div className="col-md-6">
+            <h4>Output</h4>
+            <div id="preview">
+              <p id="markdown-text" dangerouslySetInnerHTML={markdownOutput}></p>
+            </div>
+            <button
+              type="button"
+              onClick={this.copyToClipboard}
+              className="btn btn-outline-primary btn-sm"
+            >
+              Copy to clipboard
+            </button>
+          </div>
+        </main>
+        <footer>
+          Made by Andreea Năstase in Amsterdam
+          <span role="img" aria-label="Netherlands flag">
+            🇳🇱
+          </span>
+        </footer>
+      </React.Fragment>
     );
   }
 }
