57f8c80c5311d400537032f180f9afd013312821
Andreea Nastase <andreanastase@gmail.com>
2022-01-20 11:18:18 +0100

    chore: refactor to modern React

--- src/index.js
+++ src/index.js
@@ -1,74 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import marked from 'marked';
+import { marked } from 'marked';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      markdownInput:
-        '(Delete or edit this text as needed to see the preview on the right hand side)\n\n# Heading 1\n\n## Heading 2\n\nMarkdown was created by [John Gruber with help from Aaron Swartz](https://daringfireball.net/projects/markdown/)\n\nUse single backticks `to represent inline code`\n\nUse triple backticks for code blocks:\n\n```\n<script>\nalert( `Hello, world!` );\n</script>\n```\n\nUse hyphens (dashes) to indicate list items:\n\n- An item\n\n- Another item\n\n- A third item\n\nUse the greater than sign to indicate a blockquote:\n\n> This is a quoted paragraph\n\nYou can add images too:\n\n![Image of the Markdown mark](https://raw.githubusercontent.com/dcurtis/markdown-mark/master/png/66x40.png)\n\nAnd make things **strong** or _emphasize_ them.',
-    };
+const MARKDOWN_SAMPLE = '(Delete or edit this text as needed to see the preview on the right hand side)\n\n# Heading 1\n\n## Heading 2\n\nMarkdown was created by [John Gruber with help from Aaron Swartz](https://daringfireball.net/projects/markdown/)\n\nUse single backticks `to represent inline code`\n\nUse triple backticks for code blocks:\n\n```\n < script >\nalert(`Hello, world!`); \n</script >\n```\n\nUse hyphens (dashes) to indicate list items:\n\n- An item\n\n- Another item\n\n- A third item\n\nUse the greater than sign to indicate a blockquote:\n\n> This is a quoted paragraph\n\nYou can add images too:\n\n![Image of the Markdown mark](https://raw.githubusercontent.com/dcurtis/markdown-mark/master/png/66x40.png)\n\nAnd make things **strong** or _emphasize_ them.';
 
-    this.handleChange = this.handleChange.bind(this);
-    this.copyToClipboard = this.copyToClipboard.bind(this);
-  }
+const App = () => {
+  const [markdownInput, setMarkdownInput] = useState(MARKDOWN_SAMPLE);
 
-  handleChange(event) {
-    this.setState({ markdownInput: event.target.value });
-  }
+  const handleChange = (event) => {
+    setMarkdownInput(event.target.value);
+  };
 
-  async copyToClipboard() {
-    let copyText = marked(this.state.markdownInput);
-
+  const copyToClipboard = async () => {
+    const copyText = marked(markdownInput);
     try {
       await navigator.clipboard.writeText(copyText);
     } catch (error) {
       console.error('Failed to copy!', error);
     }
-  }
+  };
 
-  render() {
-    let markdownText = marked(this.state.markdownInput, { breaks: true });
-    let markdownOutput = { __html: `${markdownText}` };
+  const markdownText = marked(markdownInput, { breaks: true });
+  const markdownOutput = { __html: `${ markdownText }` };
 
-    return (
-      <React.Fragment>
-        <header className="row">
-          <h1>A simple Markdown Previewer</h1>
-        </header>
-        <main className="row">
-          <div className="col-md-6">
-            <h4>Input</h4>
-            <textarea
-              type="text"
-              id="editor"
-              value={this.state.markdownInput}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <h4>Output</h4>
-            <div id="preview" dangerouslySetInnerHTML={markdownOutput}></div>
-            <div id="copy-to-clipboard">
-              <button
-                type="button"
-                onClick={this.copyToClipboard}
-                className="btn btn-outline-primary btn-sm"
-              >
-                Copy HTML to clipboard
-              </button>
-              <p id="disclaimer">In Chrome and Firefox for now</p>
-            </div>
-          </div>
-        </main>
-        <footer>
-          <a href="https://diemkay.github.io/">Andreea Năstase</a> ・ 2020
-        </footer>
-      </React.Fragment>
-    );
-  }
-}
+  return (<>
+    <header className="row">
+      <h1>A simple Markdown Previewer</h1>
+    </header>
+    <main className="row">
+      <div className="col-md-6">
+        <h4>Input</h4>
+        <textarea
+          type="text"
+          id="editor"
+          value={markdownInput}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="col-md-6">
+        <h4>Output</h4>
+        <div id="preview" dangerouslySetInnerHTML={markdownOutput}></div>
+        <div id="copy-to-clipboard">
+          <button
+            type="button"
+            onClick={copyToClipboard}
+            className="btn btn-outline-primary btn-sm"
+          >
+            Copy HTML to clipboard
+          </button>
+          <p id="disclaimer">In Chrome and Firefox for now</p>
+        </div>
+      </div>
+    </main>
+    <footer>
+      by <a href="https://diemkay.github.io/">Andreea Năstase</a>
+    </footer>
+  </>);
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
